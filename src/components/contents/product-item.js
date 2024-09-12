330abf73bc0f8bab48440f91199374d1378c15b1
100percentibrahim <ibrahimcpersonal@gmail.com>
2024-09-12 19:58:04 +0700

    fix: fix item state

--- src/components/contents/product-item.js
+++ src/components/contents/product-item.js
@@ -2,11 +2,11 @@ import React from "react";
 import { Button } from "@ibrahimstudio/button";
 import styles from "./styles/product-item.module.css";
 
-const ProductItem = ({ isActive = false, setSelectedItem, value }) => {
+const ProductItem = ({ isActive = false, onSelect, value }) => {
   return (
     <section className={styles.productItem}>
       <b className={styles.itemLabel}>{value}</b>
-      <Button type="button" size="sm" variant={isActive ? "line" : "fill"} color={isActive ? "var(--color-primary)" : "var(--color-foreground)"} bgColor={isActive ? "transparent" : "var(--color-primary)"} buttonText={isActive ? "Terpilih" : "Pilih"} onClick={() => setSelectedItem(value)} />
+      <Button type="button" size="sm" variant={isActive ? "line" : "fill"} color={isActive ? "var(--color-primary)" : "var(--color-foreground)"} bgColor={isActive ? "transparent" : "var(--color-primary)"} buttonText={isActive ? "Terpilih" : "Pilih"} onClick={onSelect} />
     </section>
   );
 };
--- src/components/contents/product-sm.js
+++ src/components/contents/product-sm.js
@@ -9,7 +9,7 @@ const ProductSm = ({ items, selectedItem, setSelectedItem }) => {
   return (
     <section className={styles.productSm}>
       {items.map((item, index) => (
-        <ProductItem key={index} value={newPrice(item.tuition_fee)} isActive={selectedItem === item.tuition_fee} setSelectedItem={setSelectedItem} />
+        <ProductItem key={index} value={newPrice(item.tuition_fee)} isActive={selectedItem === item.tuition_fee} onSelect={() => setSelectedItem(item.tuition_fee)} />
       ))}
     </section>
   );
